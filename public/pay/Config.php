6a0f770bd6ce2ea4ec2d504e658fe1d002d20da0
Li Hui Feng <xx90ff@163.com>
2021-04-06 09:43:13 +0800

    解决bug2222

--- public/pay/Config.php
+++ public/pay/Config.php
@@ -6,7 +6,7 @@
 
     'appid' => '20212105808009', // 商户应用id
     'appsecret' => '2bfAW2n8MfMab6maRQkZw57Fh7eck8hK', // 商户MD5签名密钥
-    'gateway_url' => 'http://www.fapay.com/api/pay/submit ', // 创建订单接口
+    'gateway_url' => 'http://'.$_SERVER['HTTP_HOST'].'/api/pay/submit ', // 创建订单接口
     'paytype'=>'bipay'
     ];
 
